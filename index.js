577bd8ad928766877a7467a83bba7ef81e79d01a
Kou <84651785+Kirizakuga@users.noreply.github.com>
2025-09-25 15:31:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function solver(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    if(isNaN(a) || isNaN(b) || isNaN(c)){
+        document.getElementById("ketqua").innerHTML = "vui long nhap a,b,c";
+        return;
+    }
+    var delta = b*b - 4*a*c;
+    
+    
+    if (delta > 0){
+        document.getElementById('ketqua').innerHTML = "pt co 2 nghiem phan biet";
+        return;
+    }
+    else if(delta === 0 ){
+        document.getElementById('ketqua').innerHTML = "pt co nghiem kep";
+        return;
+    }
+    else{
+        document.getElementById('ketqua').innerHTML = "pt vo nghiem";
+        return;
+    }
+    
+}
+
+document.getElementById('solve').addEventListener('click', solver)
